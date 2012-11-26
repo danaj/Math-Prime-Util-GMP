964f77baf427db493df27f2d3289663b20827e37
Dana Jacobsen <dana@acm.org>
2012-11-26 00:33:55 -0800

    Speedup affine ECM

--- gmp_main.c
+++ gmp_main.c
@@ -1771,43 +1771,44 @@ static void _ec_add_AB(mpz_t n,
                     struct _ec_point *P3,
                     mpz_t m,
                     mpz_t t1,
-                    mpz_t t2,
-                    mpz_t delta_x,
-                    mpz_t delta_y)
+                    mpz_t t2)
 {
-  mpz_sub(t1, P2.x, P1.x);
-  mpz_tdiv_r(delta_x, t1, n);
-  mpz_sub(t1, P2.y, P1.y);
-  mpz_tdiv_r(delta_y, t1, n);
-
-  mpz_add(t1, P1.y, P2.y);
-  mpz_tdiv_r(t2, t1, n);
-
-  if ( !mpz_cmp(P1.x, P2.x) && !mpz_cmp_ui(t2, 0) ) {
-    mpz_set_ui(P3->x, 0);
-    mpz_set_ui(P3->y, 1);
-    return;
+  if (!mpz_cmp(P1.x, P2.x)) {
+    mpz_add(t2, P1.y, P2.y);
+    mpz_mod(t1, t2, n);
+    if (!mpz_cmp_ui(t1, 0) ) {
+      mpz_set_ui(P3->x, 0);
+      mpz_set_ui(P3->y, 1);
+      return;
+    }
   }
+
+  mpz_sub(t1, P2.x, P1.x);
+  mpz_mod(t2, t1, n);
+
   /* m = (y2 - y1) * (x2 - x1)^-1 mod n */
-  if (!mpz_invert(t1, delta_x, n)) {
+  if (!mpz_invert(t1, t2, n)) {
     /* There are ways we should use to handle this, but for now just bail. */
     mpz_set_ui(P3->x, 0);
     mpz_set_ui(P3->y, 1);
     return;
   }
 
-  mpz_mul(t2, t1, delta_y);
-  mpz_tdiv_r(m, t2, n);
+  mpz_sub(m, P2.y, P1.y);
+  mpz_mod(t2, m, n);        /* t2 = deltay */
+  mpz_mul(m, t1, t2);
+  mpz_mod(m, m, n);         /* m = deltay / deltax */
+
   /* x3 = m^2 - x1 - x2 mod n */
   mpz_mul(t1, m, m);
   mpz_sub(t2, t1, P1.x);
   mpz_sub(t1, t2, P2.x);
-  mpz_tdiv_r(P3->x, t1, n);
+  mpz_mod(P3->x, t1, n);
   /* y3 = m(x1 - x3) - y1 mod n */
   mpz_sub(t1, P1.x, P3->x);
   mpz_mul(t2, m, t1);
   mpz_sub(t1, t2, P1.y);
-  mpz_tdiv_r(P3->y, t1, n);
+  mpz_mod(P3->y, t1, n);
 }
 
 /* P3 = 2*P1 */
@@ -1849,16 +1850,13 @@ static int _ec_multiply(mpz_t a, UV k, mpz_t n, struct _ec_point P, struct _ec_p
 {
   int found = 0;
   struct _ec_point A, B, C;
-  mpz_t t, t2, t3, t4, t5;
+  mpz_t t, t2, t3, mult;
 
   mpz_init(A.x); mpz_init(A.y);
   mpz_init(B.x); mpz_init(B.y);
   mpz_init(C.x); mpz_init(C.y);
-  mpz_init(t);
-  mpz_init(t2);
-  mpz_init(t3);
-  mpz_init(t4);
-  mpz_init(t5);
+  mpz_init(t);   mpz_init(t2);   mpz_init(t3);
+  mpz_init_set_ui(mult, 1);  /* holds intermediates, gcd at end */
 
   mpz_set(A.x, P.x);  mpz_set(A.y, P.y);
   mpz_set_ui(B.x, 0); mpz_set_ui(B.y, 1);
@@ -1867,43 +1865,36 @@ static int _ec_multiply(mpz_t a, UV k, mpz_t n, struct _ec_point P, struct _ec_p
   while (k > 0) {
     if ( k & 1 ) {
       mpz_sub(t, B.x, A.x);
-      mpz_tdiv_r(t2, t, n);
-      mpz_gcd(d, t2, n);
-      found = (mpz_cmp_ui(d, 1) && mpz_cmp(d, n));
-      if (found)
-        break;
+      mpz_mul(t2, mult, t);
+      mpz_mod(mult, t2, n);
 
       if ( !mpz_cmp_ui(A.x, 0) && !mpz_cmp_ui(A.y, 1) ) {
         /* nothing */
       } else if ( !mpz_cmp_ui(B.x, 0) && !mpz_cmp_ui(B.y, 1) ) {
         mpz_set(B.x, A.x);  mpz_set(B.y, A.y);
       } else {
-        _ec_add_AB(n, A, B, &C, t, t2, t3, t4, t5);
+        _ec_add_AB(n, A, B, &C, t, t2, t3);
         mpz_set(B.x, C.x);  mpz_set(B.y, C.y);
       }
       k--;
     } else {
       mpz_mul_ui(t, A.y, 2);
-      mpz_tdiv_r(t2, t, n);
-      mpz_gcd(d, t2, n);
-      found = (mpz_cmp_ui(d, 1) && mpz_cmp(d, n));
-      if (found)
-        break;
+      mpz_mul(t2, mult, t);
+      mpz_mod(mult, t2, n);
 
       _ec_add_2A(a, n, A, &C, t, t2, t3);
       mpz_set(A.x, C.x);  mpz_set(A.y, C.y);
       k >>= 1;
     }
   }
+  mpz_gcd(d, mult, n);
+  found = (mpz_cmp_ui(d, 1) && mpz_cmp(d, n));
 
   mpz_tdiv_r(R->x, B.x, n);
   mpz_tdiv_r(R->y, B.y, n);
 
-  mpz_clear(t);
-  mpz_clear(t2);
-  mpz_clear(t3);
-  mpz_clear(t4);
-  mpz_clear(t5);
+  mpz_clear(mult);
+  mpz_clear(t);   mpz_clear(t2);   mpz_clear(t3);
   mpz_clear(A.x); mpz_clear(A.y);
   mpz_clear(B.x); mpz_clear(B.y);
   mpz_clear(C.x); mpz_clear(C.y);
@@ -1911,7 +1902,7 @@ static int _ec_multiply(mpz_t a, UV k, mpz_t n, struct _ec_point P, struct _ec_p
   return found;
 }
 
-int _GMP_ecm_factor(mpz_t n, mpz_t f, UV BMax, UV ncurves)
+int _GMP_ecm_factor(mpz_t n, mpz_t f, UV B1, UV ncurves)
 {
   mpz_t a;
   struct _ec_point X, Y;
@@ -1924,7 +1915,8 @@ int _GMP_ecm_factor(mpz_t n, mpz_t f, UV BMax, UV ncurves)
   mpz_init(X.x); mpz_init(X.y);
   mpz_init(Y.x); mpz_init(Y.y);
 
-  for (B = 1000; B < BMax; B *= 5) {
+  for (B = 100; B < B1*5; B *= 5) {
+    if (B*5 > 2*B1) B = B1;
     for (curve = 0; curve < ncurves; curve++) {
       mpz_urandomm(a, _randstate, n);
       mpz_set_ui(X.x, 0); mpz_set_ui(X.y, 1);
