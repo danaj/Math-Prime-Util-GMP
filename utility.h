ec0cf6ce8b3bf0fdc16ef7f35b327d14cb2be9c5
Dana Jacobsen <dana@acm.org>
2013-05-03 02:43:51 -0700

    Big updates to utility,c to add many new functions (internal C+GMP)

--- utility.h
+++ utility.h
@@ -24,12 +24,27 @@ extern UV mpz_order_ui(UV r, mpz_t n, UV limit);
 extern void poly_mod_mul(mpz_t* px, mpz_t* py, UV r, mpz_t mod, mpz_t t1, mpz_t t2, mpz_t t3);
 extern void poly_mod_pow(mpz_t *pres, mpz_t *pn, mpz_t power, UV r, mpz_t mod);
 
+extern void poly_mod(mpz_t *pres, mpz_t *pn, UV *dn, mpz_t mod);
+extern void polyz_mod(mpz_t *pres, mpz_t *pn, long *dn, mpz_t mod);
+
+extern void polyz_set(mpz_t* pr, long* dr, mpz_t* ps, long ds);
+extern void polyz_mulmod(mpz_t* pr, mpz_t* px, mpz_t *py, long *dr, long dx, long dy, mpz_t mod);
+extern void polyz_div(mpz_t *pq, mpz_t *pr, mpz_t *pn, mpz_t *pd,
+                      long *dq, long *dr, long dn, long dd);
+extern void polyz_pow_polymod(mpz_t* pres,  mpz_t* pn,  mpz_t* pmod,
+                              long *dres,   long   dn,  long   dmod,
+                              mpz_t power, mpz_t NMOD);
+extern void polyz_gcd(mpz_t* pres, mpz_t* pa, mpz_t* pb, long* dres, long da, long db, mpz_t MODN);
+
+extern void polyz_root_deg1(mpz_t root, mpz_t* pn, mpz_t NMOD);
+extern void polyz_root_deg2(mpz_t root1, mpz_t root2, mpz_t* pn, mpz_t NMOD);
+
 /* Solve x^2 + |D|y^2 = p */
 extern int cornacchia(mpz_t x, mpz_t y, mpz_t D, mpz_t p);
 /* Solve x^2 + |D|y^2 = 4p */
 extern int modified_cornacchia(mpz_t x, mpz_t y, mpz_t D, mpz_t p);
 
-/* return a Hilbert poly */
-extern UV poly_hilbert(IV D, mpz_t**T);
+/* return a class poly (Hilbert [type 1] or Weber [type 2]) */
+extern UV poly_class_poly(IV D, mpz_t**T, int* type);
 
 #endif
