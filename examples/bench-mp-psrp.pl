7db230e69f60d02296d498e93abbd3e8ea3a3d1f
Dana Jacobsen <dana@acm.org>
2014-06-17 13:48:11 -0700

    Comment out PP from benchmark

--- examples/bench-mp-psrp.pl
+++ examples/bench-mp-psrp.pl
@@ -23,7 +23,7 @@ sub test_at_digits {
   cmpthese($count,{
     'MP'      =>sub {Math::Primality::is_strong_pseudoprime($_,3) for @nums;},
     'MPU'     =>sub {Math::Prime::Util::is_strong_pseudoprime($_,3) for @nums;},
-    'MPU PP'  =>sub {Math::Prime::Util::PP::miller_rabin($_,3) for @nums;},
+    #'MPU PP'  =>sub {Math::Prime::Util::PP::is_strong_pseudoprime($_,3) for @nums;},
     'MPU GMP' =>sub {Math::Prime::Util::GMP::is_strong_pseudoprime($_,3) for @nums;},
   });
 }
