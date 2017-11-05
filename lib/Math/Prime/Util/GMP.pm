06e134b59421ebfa9ae7ec776410db97d55ef55c
Dana Jacobsen <dana@acm.org>
2017-11-05 08:42:44 -0800

    Bump version number

--- lib/Math/Prime/Util/GMP.pm
+++ lib/Math/Prime/Util/GMP.pm
@@ -5,7 +5,7 @@ use Carp qw/croak confess carp/;
 
 BEGIN {
   $Math::Prime::Util::GMP::AUTHORITY = 'cpan:DANAJ';
-  $Math::Prime::Util::GMP::VERSION = '0.47';
+  $Math::Prime::Util::GMP::VERSION = '0.48';
 }
 
 # parent is cleaner, and in the Perl 5.10.1 / 5.12.0 core, but not earlier.
@@ -198,7 +198,7 @@ Math::Prime::Util::GMP - Utilities related to prime numbers and factoring, using
 
 =head1 VERSION
 
-Version 0.47
+Version 0.48
 
 
 =head1 SYNOPSIS
