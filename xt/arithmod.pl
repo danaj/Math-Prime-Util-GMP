47867b8cc427fbc0c93c338604f686943f3b0135
Dana Jacobsen <dana@acm.org>
2020-08-09 19:12:45 +0700

    Turn warns into die, add three extra tests for completeness

--- xt/arithmod.pl
+++ xt/arithmod.pl
@@ -11,7 +11,7 @@ for my $p (1 .. 20) {       my $P = Math::BigInt->new($p);
   for my $a (-20..20) {     my $A = Math::BigInt->new($a);
     for my $b (-20..20) {   my $B = Math::BigInt->new($b);
       if (1) {
-        warn "addmod($a,$b,$p)" unless addmod($a,$b,$p) == ($A + $B) % $P;
+        die "addmod($a,$b,$p)" unless addmod($a,$b,$p) == ($A + $B) % $P;
       }
       if (1) {
         die "mulmod($a,$b,$p)" unless mulmod($a,$b,$p) == ($A * $B) % $P;
@@ -19,21 +19,25 @@ for my $p (1 .. 20) {       my $P = Math::BigInt->new($p);
       if (1) {
         my $pm1 = powmod($a,$b,$p);
         my $pm2 = $A->copy->bmodpow($B,$P);
-        warn "powmod($a,$b,$p) unequal" if defined $pm1 && $pm1 != $pm2;
-        warn "powmod($a,$b,$p) missing" if !defined $pm1 && $pm2->is_int();
+        die "powmod($a,$b,$p) unequal" if defined $pm1 && $pm1 != $pm2;
+        die "powmod($a,$b,$p) missing" if !defined $pm1 && $pm2->is_int();
       }
       if (1) {
         # Both Pari and Math::BigInt say:
         #   divmod(a,0,p) = 0 if p=1, range error otherwise
         my $dm1 = divmod($a,$b,$p);
         my $dm2 = $B->copy->bmodinv($P)->bmul($A)->bmod($P);
-        warn "divmod($a,$b,$p) unequal" if defined $dm1 && $dm1 != $dm2;
-        warn "divmod($a,$b,$p) missing" if !defined $dm1 && $dm2->is_int();
+        die "divmod($a,$b,$p) unequal" if defined $dm1 && $dm1 != $dm2;
+        die "divmod($a,$b,$p) missing" if !defined $dm1 && $dm2->is_int();
       }
       # invmod
       # sqrtmod
     }
   }
 }
+
+die "Bad divmod 1,1,1000000001" unless divmod(1,1,1000000001) == 1;
+die "Bad divmod 43,1,1000000001" unless divmod(43, 1, 1000000001) == 43;
+die "Bad divmod 127,1,97" unless divmod(127, 1, 97) == 30;
 
 # TODO test handle zero P
