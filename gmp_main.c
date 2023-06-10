e6f2c4086685904adbbed90d137cb47722d9f389
Trizen <trizen@protonmail.com>
2023-06-10 05:43:57 +0000

    Fix is_carmichael with random large bases
    Check `base^n == base (mod n)` instead of `base^(n-1) == 1 (mod n)`, which works even when `base` is not coprime to `n`.

--- gmp_main.c
+++ gmp_main.c
@@ -1224,8 +1224,8 @@ int is_carmichael(mpz_t n)
       mpz_sub_ui(t, n, 4);
       mpz_isaac_urandomm(base, t);
       mpz_add_ui(base, base, 3);    /* random base between 3 and n-2 */
-      mpz_powm(t, base, nm1, n);
-      res = (mpz_cmp_ui(t, 1) == 0);  /* if base^(n-1) mod n != 1, fail */
+      mpz_powm(t, base, n, n);
+      res = (mpz_cmp(t, base) == 0);  /* if base^n mod n != base, fail */
     }
 
   } else {                              /* Deterministic test (factor n) */
