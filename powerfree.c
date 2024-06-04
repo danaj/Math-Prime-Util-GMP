471a6c336a8d810e4919bdfb207c3ab36a12c24f
Dana Jacobsen <dana@acm.org>
2024-06-04 19:21:37 +0700

    Much faster powerfree_count and nth_powerfree

--- powerfree.c
+++ powerfree.c
@@ -4,7 +4,6 @@
 #include "powerfree.h"
 #include "factor.h"
 #include "real.h"
-#include "prime_iterator.h"
 #include "utility.h"
 #define FUNC_isqrt 1
 #define FUNC_ipow 1
@@ -87,137 +86,7 @@ void prev_powerfree(mpz_t prev, mpz_t n, uint32_t k)
   mpz_clear(N);
 }
 
-#if 1
-/*
- * Powerfree count really needs a range moebius for performance.
- */
 
-void powerfree_count(mpz_t count, mpz_t n, uint32_t k)
-{
-#if 0
-  mpz_t t, i, nk, c;
-
-  if (k < 2 || mpz_sgn(n) <= 0) {
-    mpz_set_ui(count, (mpz_cmp_ui(n,1) >= 0));
-    return;
-  }
-  if (mpz_cmp_ui(n, 4) < 0) {
-    mpz_set(count, n);
-    return;
-  }
-
-  mpz_init(t);
-  mpz_init(i);
-  mpz_init(nk);
-  mpz_root(nk, n, k);
-  mpz_init_set(c, n);
-
-  for (mpz_set_ui(i,2);  mpz_cmp(i,nk) <= 0;  mpz_add_ui(i,i,1)) {
-    int m = moebius(i);
-    if (m == 0) continue;
-    mpz_pow_ui(t, i, k);
-    mpz_fdiv_q(t, n, t);
-    if (m > 0)  mpz_add(c, c, t);
-    else        mpz_sub(c, c, t);
-  }
-  mpz_set(count, c);
-  mpz_clear(c);  mpz_clear(nk);  mpz_clear(i);  mpz_clear(t);
-#else
-  mpz_t t, i, L1, nk, c;
-  signed long c1;
-
-  if (k < 2 || mpz_sgn(n) <= 0) {
-    mpz_set_ui(count, (mpz_cmp_ui(n,1) >= 0));
-    return;
-  }
-  if (mpz_cmp_ui(n, 4) < 0) {
-    mpz_set(count, n);
-    return;
-  }
-
-  mpz_init(t);
-  mpz_init(i);
-  mpz_init(L1);
-  mpz_init(nk);
-  mpz_init(c);
-
-  mpz_fdiv_q_2exp(t, n, 1);
-  mpz_root(L1, t, k);
-  mpz_root(nk, n, k);
-  mpz_init_set_ui(c, 0);
-
-  for (mpz_set_ui(i,2);  mpz_cmp(i,L1) <= 0;  mpz_add_ui(i,i,1)) {
-    int m = moebius(i);
-    if (m != 0) {
-      mpz_pow_ui(t, i, k);
-      mpz_fdiv_q(t, n, t);
-      if (m > 0)  mpz_add(c, c, t);
-      else        mpz_sub(c, c, t);
-    }
-  }
-  for (c1 = 0;  mpz_cmp(i,nk) <= 0;  mpz_add_ui(i,i,1))
-    c1 += moebius(i);
-  mpz_set(count, n);
-  mpz_add(count, count, c);
-  if (c1 >= 0) mpz_add_ui(count, count, (unsigned long) c1);
-  else         mpz_sub_ui(count, count, (unsigned long) (-c1));
-  mpz_clear(c);  mpz_clear(nk);  mpz_clear(L1);  mpz_clear(i);  mpz_clear(t);
-#endif
-}
-#else
-
-#define P_GT_LO(f,p,lo)  ( ((f)>=(lo)) ? (f) : (lo)+(((p)-((lo)%(p)))%(p)) )
-
-/* Return a char array with lo-hi+1 elements. mu[k-lo] = µ(k) for k = lo .. hi.
- * It is the callers responsibility to call Safefree on the result. */
-signed char* range_moebius(UV lo, UV hi)
-{
-  signed char* mu;
-  UV p, i, sqrtn = isqrt(hi), count = hi-lo+1;
-
-  /* Kuznetsov indicates that the Deléglise & Rivat (1996) method can be
-   * modified to work on logs, which allows us to operate with no
-   * intermediate memory at all.  Same time as the D&R method, less memory. */
-  unsigned char logp;
-  UV nextlog, nextlogi;
-
-  if (hi < lo) croak("range_mobius error hi %"UVuf" < lo %"UVuf"\n", hi, lo);
-
-  Newz(0, mu, count, signed char);
-  if (sqrtn*sqrtn != hi && sqrtn < (UVCONST(1)<<(BITS_PER_WORD/2))-1) sqrtn++;
-
-  {
-    PRIME_ITERATOR(iter);
-
-    logp = 1; nextlog = 3; /* 2+1 */
-    for (p = 2; p <= sqrtn; p = prime_iterator_next(&iter)) {
-      UV p2 = p*p;
-      if (p > nextlog) {
-        logp += 2;   /* logp is 1 | ceil(log(p)/log(2)) */
-        nextlog = ((nextlog-1)*4)+1;
-      }
-      for (i = P_GT_LO(p, p, lo); i >= lo && i <= hi; i += p)
-        mu[i-lo] += logp;
-      for (i = P_GT_LO(p2, p2, lo); i >= lo && i <= hi; i += p2)
-        mu[i-lo] = 0x80;
-    }
-    prime_iterator_destroy(&iter);
-  }
-
-  logp = log2_ui(lo);
-  nextlogi = (UVCONST(2) << logp) - lo;
-  for (i = 0; i < count; i++) {
-    unsigned char a = mu[i];
-    if (i >= nextlogi) nextlogi = (UVCONST(2) << ++logp) - lo;
-    if (a & 0x80)       { a = 0; }
-    else if (a >= logp) { a =  1 - 2*(a&1); }
-    else                { a = -1 + 2*(a&1); }
-    mu[i] = a;
-  }
-  if (lo == 0)  mu[0] = 0;
-
-  return mu;
-}
 
 static UV squarefree_count_ui(UV n)
 {
@@ -287,11 +156,10 @@ static UV powerfree_count_ui(UV n, uint32_t k)
   return count;
 }
 
-
 void powerfree_count(mpz_t count, mpz_t n, uint32_t k)
 {
-  mpz_t t, i, L1, nk, c;
-  signed long int c1;
+  mpz_t t, i, c, c1, nk;
+  static const int PERF_MM = 4;  /* Larger = more moebius, less mertens */
 
   if (k < 2 || mpz_sgn(n) <= 0) {
     mpz_set_ui(count, (mpz_cmp_ui(n,1) >= 0));
@@ -302,38 +170,77 @@ void powerfree_count(mpz_t count, mpz_t n, uint32_t k)
     return;
   }
   if (mpz_fits_uv_p(n)) {
-    UV c = powerfree_count_ui(mpz_get_uv(n), k);
-    mpz_set_uv(count, c);
+    UV cnt = powerfree_count_ui(mpz_get_uv(n), k);
+    mpz_set_uv(count, cnt);
     return;
   }
 
   mpz_init(t);
   mpz_init(i);
-  mpz_init(L1);
+  mpz_init_set_ui(c,  0);
+  mpz_init_set_ui(c1, 0);
   mpz_init(nk);
-  mpz_init(c);
-
-  mpz_fdiv_q_2exp(t, n, 1);
-  mpz_root(L1, t, k);
   mpz_root(nk, n, k);
-  mpz_init_set_ui(c, 0);
 
-  gmp_printf("   nk %Zd   L1 %Zd\n", nk, L1);
-  if (0 && mpz_cmp_ui(nk, 1e8) < 0) {
-    unsigned long int j, nku = mpz_get_ui(nk), L1u = mpz_get_ui(L1);
-    signed char* mu = range_moebius(0, nku);
-    for (j = 2; j <= L1u; j++) {
-      if (mu[j] != 0) {
-        mpz_ui_pow_ui(t, j, k);
-        mpz_fdiv_q(t, n, t);
-        if (mu[j] > 0) mpz_add(c, c, t);
-        else           mpz_sub(c, c, t);
+  if (mpz_sizeinbase(nk,2) <= 50) {
+    unsigned long int j, LA, wbeg, wend, nku = mpz_get_ui(nk);
+    unsigned long int A = isqrt(nku) / PERF_MM;
+    signed char* mu;
+
+    if (A <= 1) { A = 1; LA = nku; }
+    else        { mpz_fdiv_q_ui(t,n,A); mpz_root(t,t,k); LA = mpz_get_ui(t); }
+    /* gmp_printf("   A %lu  LA %lu\n",A,LA); */
+
+    /* Dense region.  Use a windowed ranged moebius and sum each each value. */
+    for (wbeg = 2;  wbeg <= LA;  wbeg = wend+1) {
+      wend = ((LA-wbeg) > 10000000UL)  ?  wbeg + 10000000UL - 1  :  LA;
+      mu = range_moebius(wbeg, wend);
+      for (j = wbeg; j <= wend; j++) {
+        if (mu[j-wbeg] != 0) {
+          mpz_ui_pow_ui(t, j, k);
+          mpz_fdiv_q(t, n, t);
+          if (mu[j-wbeg] > 0) mpz_add(c, c, t);
+          else                mpz_sub(c, c, t);
+        }
       }
+      Safefree(mu);
     }
-    for (c1 = 0; j <= nku; j++)
-      c1 += mu[j];
-    Safefree(mu);
+    /* printf("   MOEBIUS DONE\n"); fflush(stdout); */
+
+    /* Sparse region.  Many of the j from LA to nk will result in the same
+     * value for n/(j^k).  We walk the possible values instead, multiplying
+     * by the moebius values.  Furthermore, we use a fast caching Mertens
+     * function instead of summing the moebius values directly.
+     */
+    if (A > 1) {
+      void *mctx = hmertens_create(nku);
+      signed long int *M;
+
+      New(0, M, A+1, signed long int);
+      M[0] = 0;
+      M[1] = hmertens_value(mctx, nku);
+      for (j = 2; j <= A; j++) {
+        mpz_fdiv_q_ui(t, n, j);
+        mpz_root(t, t, k);
+        M[j] = hmertens_value(mctx, mpz_get_ui(t));
+      }
+      hmertens_destroy(mctx);
+
+      for (j = 2; j <= A; j++) {
+        mpz_set_si(t, M[j-1] - M[j]);
+        mpz_mul_ui(t, t, j-1);
+        mpz_add(c1, c1, t);
+      }
+      Safefree(M);
+    }
+
   } else {
+    mpz_t L1;
+    signed long int c1i;
+
+    mpz_init(L1);
+    mpz_fdiv_q_2exp(t, n, 1);
+    mpz_root(L1, t, k);
     for (mpz_set_ui(i,2);  mpz_cmp(i,L1) <= 0;  mpz_add_ui(i,i,1)) {
       int m = moebius(i);
       if (m != 0) {
@@ -343,16 +250,18 @@ void powerfree_count(mpz_t count, mpz_t n, uint32_t k)
         else        mpz_sub(c, c, t);
       }
     }
-    for (c1 = 0;  mpz_cmp(i,nk) <= 0;  mpz_add_ui(i,i,1))
-      c1 += moebius(i);
+    mpz_clear(L1);
+    for (c1i = 0;  mpz_cmp(i,nk) <= 0;  mpz_add_ui(i,i,1))
+      c1i += moebius(i);
+    mpz_set_si(c1, c1i);
   }
   mpz_set(count, n);
   mpz_add(count, count, c);
-  if (c1 >= 0) mpz_add_ui(count, count, (unsigned long) c1);
-  else         mpz_sub_ui(count, count, (unsigned long) (-c1));
-  mpz_clear(c);  mpz_clear(nk);  mpz_clear(L1);  mpz_clear(i);  mpz_clear(t);
+  mpz_add(count, count, c1);
+  mpz_clear(nk);  mpz_clear(c);  mpz_clear(c1);  mpz_clear(i);  mpz_clear(t);
 }
-#endif
+
+
 
 void nth_powerfree(mpz_t nth, mpz_t n, uint32_t k)
 {
@@ -389,7 +298,7 @@ void nth_powerfree(mpz_t nth, mpz_t n, uint32_t k)
     if (mpz_sizeinbase(n,10) <= 16) break;
     mpz_sub(diff, n, count);
     /* gmp_printf("qk %Zd  count %Zd  diff %Zd\n", v, count, diff); fflush(stdout); */
-    if (mpz_cmpabs_ui(diff, 20) <= 0) break;
+    if (mpz_cmpabs_ui(diff, 40) <= 0) break;
 
     /* Adjust difference for expected number of k-powerfree values */
     mpf_set_z(fqk,diff);
